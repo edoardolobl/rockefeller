b74d27b58fa0d1f6356fc4a38e024ab6554f815d
Edoardo <edoardo.lobl@gmail.com>
2015-07-29 16:45:00 -0400

    Tinnying the code.

--- SequencesDB/lib/SequencesDB/Schema/ResultSet/Library.pm
+++ SequencesDB/lib/SequencesDB/Schema/ResultSet/Library.pm
@@ -6,34 +6,29 @@ use feature 'say';
 use base 'DBIx::Class::ResultSet';
 
 sub search_library {
-	my ($self, $user_input, $column, $type ) = @_;
+    my ( $self, $user_input, $column, $type ) = @_;
 
-	
-	my $where = {};
-	if ( $type =~ /equal/i) {
-		$where =  { $column  => $user_input }  ;
-	}
-	elsif ( $type =~ /similar/i ){
-		$where = {  $column => { like => "%$user_input%" } };
-	}
+    my $where = {};
+    if ( $type =~ /equal/i ) {
+        $where = { $column => $user_input };
+    }
+    elsif ( $type =~ /similar/i ) {
+        $where = { $column => { like => "%$user_input%" } };
+    }
 
-	my $rs = $self->search(
-		$where,
-		{
-			prefetch => [ 
-					'organism_organism',
-					'tissue_tissue',
-					'cell_cell', 
-					'experiment_experiment', 
-					'background_background', 
-					'method_method', 
-					'antibody_antibody', 
-					'sequencer_sequencer', 
-					'files_inner' 
-			],
-		}
-	);
-	return $rs;
+    my $rs = $self->search(
+        $where,
+        {
+            prefetch => [
+                'organism_organism',     'tissue_tissue',
+                'cell_cell',             'experiment_experiment',
+                'background_background', 'method_method',
+                'antibody_antibody',     'sequencer_sequencer',
+                'files_inner'
+            ],
+        }
+    );
+    return $rs;
 }
 
 1;
